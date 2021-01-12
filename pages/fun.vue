6d67814f12ae197e03accfb06ce7964d0704f1e0
win <winlwinoocu@gmail.com>
2021-01-12 09:42:01 +0630

    first commit

---
+++ pages/fun.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1 class="red">Hello {{ name }}!</h1>
+    <LazyMountainsList v-if="show" />
+    <button v-if="!show" @click="showList()">Show List</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // page properties go here
+  data() {
+    return {
+        name: 'test',
+        show: false,
+    };
+  },
+  methods: {
+    showList() {
+      this.show = true;
+    },
+  },
+};
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
